758db0274358896e16721523ba4cb3985527532e
SVA2021 <sofronov.vit@gmail.com>
2023-01-14 12:24:27 +0400

    create component BlockButton

---
+++ components/BlockButton/BlockButton.tsx
@@ -0,0 +1,24 @@
+import {IBlock} from '@/types';
+import React, {FC} from 'react';
+import s from './BlockButton.module.scss';
+import {Ubuntu} from '@next/font/google';
+import cn from 'classnames';
+import {getBlockButtonName} from '@/utils';
+
+interface BlockButtonProps {
+  block: IBlock
+  clickHandler: () => void
+}
+
+const ubuntu = Ubuntu({subsets: ['cyrillic'], weight: ['500']});
+
+export const BlockButton: FC<BlockButtonProps> = ({block, clickHandler}) => {
+  return (
+    <button
+      className={cn(s.button, ubuntu.className, block.status ? s.button__active : '')}
+      onClick={() => clickHandler()}
+    >
+      {getBlockButtonName(block.block)}
+    </button>
+  );
+};
