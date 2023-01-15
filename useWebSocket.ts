bb542de576b7d9b9edf30b21a170853c54e33d66
SVA2021 <sofronov.vit@gmail.com>
2023-01-15 12:19:40 +0400

    upd add main methods

--- useWebSocket.ts
+++ useWebSocket.ts
@@ -4,39 +4,59 @@ import {ISocketRes} from './types';
 function useWebSocket(defaultSocket: WebSocket | null) {
 
   const [message, setMessage] = useState<ISocketRes | null>(null);
+  const [error, setError] = useState<any>(null);
 
   useEffect(() => {
-    if (defaultSocket && defaultSocket.onmessage) {
-      defaultSocket.onmessage = ((e) => setMessage(JSON.parse(e.data)));
+    try {
+      if (defaultSocket) {
+        defaultSocket.onmessage = ((e) => {
+          console.log(JSON.parse(e.data))
+          setMessage(JSON.parse(e.data))
+        });
+      }
+    } catch (e) {
+      console.log(e);
+      setError(e);
     }
   }, [defaultSocket])
 
-  const subscribe = useCallback((name: string) => {
+  const subscribe = useCallback((block: string) => {
     if (defaultSocket) {
-      defaultSocket.send(JSON.stringify({"command": "subscribe", "block": name}));
+      defaultSocket.send(JSON.stringify({"command": "subscribe", "block": block}));
+      console.log(`[subscribe] sent to block: ${block}`)
     } else {
       console.log('websocket is null')
     }
   }, [defaultSocket]);
 
-  const unsubscribe = useCallback((name: string) => {
+  const unsubscribe = useCallback((block: string) => {
     if (defaultSocket) {
-      defaultSocket.send(JSON.stringify({"command": "subscribe", "block": name}));
+      defaultSocket.send(JSON.stringify({"command": "subscribe", "block": block}));
+      console.log(`[unsubscribe] sent to block: ${block}`)
     } else {
       console.log('websocket is null')
     }
   }, [defaultSocket]);
 
-  // {"command": "focus", "block": "block1", "field": "lname"}
-  // const message = defaultSocket !== null ? defaultSocket.onmessage((e: MessageEvent) => JSON.parse(e.data)) : null;
-  //     // socket.send(JSON.stringify({"command": "subscribe", "block": block.block}));
-  // const [value, setValue] = useState(!!defaultValue)
+  const setFocus = useCallback((block: string, field: string) => {
+    if (defaultSocket) {
+      console.log(`[focus] sent to block: ${block}, field: ${field}`)
+      defaultSocket.send(JSON.stringify({"command": "focus", "block": block, "field": field}));
+    } else {
+      console.log('websocket is null')
+    }
+  }, [defaultSocket]);
 
+  const setBlur = useCallback((block: string, field: string) => {
+    if (defaultSocket) {
+      console.log(`[blur] sent to block: ${block}, field: ${field}`)
+      defaultSocket.send(JSON.stringify({"command": "blur", "block": block, "field": field}));
+    } else {
+      console.log('websocket is null')
+    }
+  }, [defaultSocket]);
 
-  // const toggle = useCallback(() => setValue(x => !x), [])
-
-  // return [value, toggle, setValue]
-  return {message, subscribe, unsubscribe, }
+  return {message, error, subscribe, unsubscribe, setFocus, setBlur}
 }
 
 export default useWebSocket;
